c397be6b120e413dc867f5e1c2ee91270281212c
DanielaSkip <SkiporeDanielaa@gmail.com>
2024-12-10 12:35:28 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        sum( a:12 b: 15);
+        sum( 12, 15);
 
     }
         public static void sum (int a , int b){
